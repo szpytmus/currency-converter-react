1af11dd15e0a43d8dd4454dd4afc2c498947032e
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-07-21 12:26:46 +0200

    Container & Header components

---
+++ src/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css'
+
+const Header = ({title}) => (
+    <header>
+        <h1 className="header">{title}</h1>
+    </header>
+);
+
+export default Header;
