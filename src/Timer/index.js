8062cc66bd238b2ea4e2e51582341a54dcce28b2
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-08-10 17:35:56 +0200

    Timer update

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import "./style.css";
 
+const formatDate = (date) => date.toLocaleDateString("en-GB", {
+    month: "long",
+    weekday: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+});
+
+
+
 const Timer = () => {
 
-    const [timeDateNow, setTimeDateNow] = useState();
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-    
+
         const intervalId = setInterval(() => {
-
-            const datePattern = {
-                month: "long", 
-                weekday: "long", 
-                day: "numeric", 
-                year: "numeric", 
-                hour:"numeric", 
-                minute:"numeric", 
-                second:"numeric"
-               };
-
-            const newDate = new Date().toLocaleDateString("en-GB", datePattern);
-            setTimeDateNow(newDate);
+            setDate(new Date());
         }, 1000);
 
         return () => {
             clearInterval(intervalId)
         };
 
-    },[]);
+    }, []);
 
-    return(
-        <div className="timer">Today is: {timeDateNow}</div>
+    return (
+        <div className="timer">Today is: {""}{formatDate(date)}</div>
     )
 };
 
